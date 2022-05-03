810561b06219c12b0ca468ecf1fd4617782ecdc1
Hombre2014 <chamkoriyski@gmail.com>
2022-05-03 23:10:14 +0200

    Ste 5 completed. Greeting React component rendered!

---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,16 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Greeting extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Greeting Message: {this.props.greetingMessage}
+      </React.Fragment>
+    );
+  }
+}
+
+Greeting.propTypes = {
+  greetingMessage: PropTypes.string
+};
+export default Greeting
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: 'json' } do
     get 'greetings', to: 'greetings#index'
   end
-  # root "articles#index"
+  root "static#index"
 end
